4e32fdc4d1d7240fa9343f36aa61698fcbc1d8c5
harinivithan4287 <138183456+harinivithan4287@users.noreply.github.com>
2023-07-25 13:43:10 +0530

    Update

--- index(25-7[JS]).js
+++ index(25-7[JS]).js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 console.log("Hi Hari");
 //single line command
 /*
@@ -11,18 +10,4 @@ let tech=["website","backend"];
 console.log(name);
 console.log(age);
 console.log(completedcollege);
-=======
-console.log("Hi Hari");
-//single line command
-/*
-multi line command
-*/
-let name="Hari";
-let age =18;
-let completedcollege=false;
-let tech=["website","backend"];
-console.log(name);
-console.log(age);
-console.log(completedcollege);
->>>>>>> 075d0532db3bcf722e217083617f9d6e1d8ef33c
 console.log(tech);
