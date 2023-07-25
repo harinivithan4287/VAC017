ae7a0e97140edd0fcb94b0cc1c537d5aed555a5b
harinivithan <harinivithan@gmail.com>
2023-07-25 13:15:45 +0530

    Update

---
+++ index(25-7[JS]).js
@@ -0,0 +1,13 @@
+console.log("Hi Hari");
+//single line command
+/*
+multi line command
+*/
+let name="Hari";
+let age =18;
+let completedcollege=false;
+let tech=["website","backend"];
+console.log(name);
+console.log(age);
+console.log(completedcollege);
+console.log(tech);
